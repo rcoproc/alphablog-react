0ebccfecf1d3d6b41c21bb4de55abc52bd794553
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-02-23 23:34:22 -0300

    Added Article component

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -4,7 +4,7 @@ class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
-        Message: {this.props.greeting}
+        Greeting: {this.props.greeting}
       </React.Fragment>
     );
   }
